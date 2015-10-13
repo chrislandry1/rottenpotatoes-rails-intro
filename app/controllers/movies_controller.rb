bb8c9d2003a4b80ad43c7a4859d8e922be0e50d5
chrislandry1 <clandry@citadel.edu>
2015-10-13 22:01:57 +0000

    rails hw part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,33 +8,46 @@ class MoviesController < ApplicationController
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    rating = params[:rating]
     @movie = Movie.find(id) # look up movie by unique ID
-    @movie = Movie.find(rating)
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @all_ratings = ['G','PG','PG-13','R']
-    #@all_ratings = Movie.all_ratings
+    #@all_ratings = ['G','PG','PG-13','R']
+    @all_ratings = Movie.all_ratings
     
     #highlight code from dr. verdicchio, haml.html code from dr. v also
-    @sort = params[:sort]
+    @sort = params[:sort] || session[:sort]
     if @sort == 'title'
         @title_hilite = 'hilite'
+        ordering = {:title => :asc}
     elsif @sort == "release_date"
         @date_hilite = 'hilite'
+        ordering = {:release_date => :asc}
     end
     
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
     
-    #rating = params[:rating]
-    #@movies = Movie.all
-    #http://railscasts.com/episodes/228-sortable-table-columns?view=asciicast
-    #@movies = Movie.order(params[:sort])
-    #we need to sort this instead of returning everything using .where
-    #@movies = Movie.where(rating: @selected_ratings)
-    @movies = Movie.where(rating: 'G')
-    #need to use hash to deal with multiple boxes checked
+    # if {}, we need to create a fake ratings hash with everything "checked"
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    end
+    #if there are things in params set session to these params
+    # else if params/session differ on :sort or :ratings
+    if(params[:ratings] != session[:ratings]) || (params[:sort] != session[:sort])
+    # store sort info into session[:sort]
+    session[:sort] = @sort
+    # store selected ratings into session[:ratings]
+    session[:ratings] = @selected_ratings
+    flash.keep
+    # redirect_to index page after setting sort and ratings params
+    redirect_to :sort => @sort, :ratings => @selected_ratings and return
+    end
+    #end
+    
+    #rating = params[:ratings]
+    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+    
   end
 
   def new
